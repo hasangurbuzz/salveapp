cb473cf93ea0a82a31e91ee24d1f50643aa45479
Hasan Gürbüz <hasgrbz@gmail.com>
2023-12-27 04:11:56 +0300

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
 import Header from "@/app/_components/Header";
+import {Analytics} from "@vercel/analytics/react";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <main>
             <Header/>
             {children}
+            <Analytics/>
         </main>
         </body>
         </html>
