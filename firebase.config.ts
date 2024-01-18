7905a41e614d511d84a61adb85149edc2875d5db
Hasan Gürbüz <hasgrbz@gmail.com>
2024-01-18 22:39:28 +0300

    env vars

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyBlBoKbYDdaF9SWq0a9K2Zht7ivT95pJgg",
-    authDomain: "salve-dd1eb.firebaseapp.com",
-    databaseURL: "https://salve-dd1eb-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "salve-dd1eb",
-    storageBucket: "salve-dd1eb.appspot.com",
-    messagingSenderId: "911566330843",
-    appId: "1:911566330843:web:617fc42a08433bd79ee5fe",
-    measurementId: "G-3F9GXWGG94"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DB_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
